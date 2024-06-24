cdebd0cb16d9bebfcdc3fd096e660b689be86228
Lakksh Tyagi <lakksh.tyagi22@gmail.com>
2024-06-24 18:01:33 +0530

    Solving error regarding JSON response data

--- src/components/MeditationSounds.js
+++ src/components/MeditationSounds.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './MeditationSounds.css';
 
+// Import local MP3 files
+import thunderSound from './thunder.mp3';
+import windSound from './wind.flac';
+import animalsSound from './jungle.flac';
+
 const MeditationSounds = () => {
   const [sounds, setSounds] = useState([
     { id: 1, soundName: "WATER", soundUrl: "https://oddwiring.com/archive/websites/mndev/resources/sounds/Boiling%20and%20Bubbling/de_leau_sur_le_feu.wav" },
-    { id: 2, soundName: "THUNDER", soundUrl: "http://www.ringelkater.de/Sounds/2geraeusche_gegenst/donner.wav" },
-    { id: 3, soundName: "WIND", soundUrl: "http://web.tiscali.it/gherda-wolit/natura/vento.wav" },
-    { id: 4, soundName: "ANIMALS", soundUrl: "http://www.zimba.kaeregaard.dk/lyd/nightdog.wav" },
+    { id: 2, soundName: "THUNDER", soundUrl: thunderSound },
+    { id: 3, soundName: "WIND", soundUrl: windSound },
+    { id: 4, soundName: "ANIMALS", soundUrl: animalsSound },
     { id: 5, soundName: "FIRE", soundUrl: "https://gamekill.cz/cstrike/sound/brn.wav" },
     { id: 6, soundName: "DROPLETS", soundUrl: "https://www.pachd.com/sfx/water_drops2.wav" }
   ]);
@@ -24,7 +29,10 @@ const MeditationSounds = () => {
           console.error('Invalid data received:', response.data);
         }
       })
-      .catch(error => console.error(error));
+      .catch(error => {
+        console.error('Error fetching meditation sounds:', error);
+        // Handle error state or fallback if necessary
+      });
   }, []);
 
   const playSound = (soundUrl, id) => {
